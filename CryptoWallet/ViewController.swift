52563285a7f0b459767b57be2c22dcbdd4435205
kot lena <aternetas@gmail.com>
2025-07-09 21:40:00 +0300

    init

---
+++ CryptoWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoWallet
+//
+//  Created by f f on 09.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
