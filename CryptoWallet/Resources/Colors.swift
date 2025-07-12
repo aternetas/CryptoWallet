79ff47737739498c99b9effadd1a8ad796993cd5
kot lena <aternetas@gmail.com>
2025-07-12 18:16:23 +0300

    add UIColor's extension

---
+++ CryptoWallet/Resources/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  CryptoWallet
+//
+//  Created by aternetas on 12.07.2025.
+//
+
+import UIKit
+
+extension UIColor {
+    static var white = UIColor(resource: .white)
+    static var gray = UIColor(resource: .gray)
+    static var black = UIColor(resource: .black)
+}
