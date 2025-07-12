84542889060c8243a0b8f2652f5057b27a101cc0
kot lena <aternetas@gmail.com>
2025-07-12 16:51:49 +0300

    add BaseViewController&BaseViewModel

---
+++ CryptoWallet/UI/Base/BaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseViewController.swift
+//  CryptoWallet
+//
+//  Created by aternetas on 12.07.2025.
+//
+
+import UIKit
+
+class BaseViewController<VM: BaseViewModel>: UIViewController {
+    var viewModel: VM!
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ CryptoWallet/UI/Base/BaseViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  BaseViewModel.swift
+//  CryptoWallet
+//
+//  Created by aternetas on 12.07.2025.
+//
+
+import Foundation
+
+class BaseViewModel {}
