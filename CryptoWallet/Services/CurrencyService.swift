b9c15e568721700525ab0321e4e8b149eea6ab9b
kot lena <aternetas@gmail.com>
2025-07-21 16:22:28 +0300

    expand Currency model

--- CryptoWallet/Services/CurrencyService.swift
+++ CryptoWallet/Services/CurrencyService.swift
@@ -12,21 +12,24 @@ final class CurrencyService {
     
     private let repository: CurrencyRepository
     
+    private(set) var currencies: [Currency] = []
+    
     init(repository: CurrencyRepository) {
         self.repository = repository
     }
     
     func getData(completionHandler: @escaping (Result<[Currency], Error>) -> Void) {
-        repository.getData { response in
+        repository.getData { [weak self] response in
+            guard let self else { return }
             switch response {
             case .success(let response):
                 let names: Set<String> = ["Bitcoin", "Ethereum", "XRP", "TRON", "LUNA", "Dogecoin", "Tether", "Stellar", "Cardano"]
                 
-                completionHandler(.success(
-                    response.data
-                        .filter { names.contains($0.name) }
-                        .map { Currency(dto: $0) }
-                ))
+                currencies = response.data
+                    .filter { names.contains($0.name) }
+                    .map { Currency(dto: $0) }
+                
+                completionHandler(.success(currencies))
             case .failure(let error):
                 completionHandler(.failure(error))
             }
--- CryptoWallet/Services/Models/Currency.swift
+++ CryptoWallet/Services/Models/Currency.swift
@@ -14,14 +14,18 @@ struct Currency {
     let media: CurrencyMedia
     let priceUSD: Double
     let percentChangeUSDLast24Hours: Double
+    let currentCapitalizationUSD: Double
+    let currentSupply: Double
     
-    init(id: String, name: String, symbol: String, media: CurrencyMedia, priceUSD: Double, percentChangeUSDLast24Hours: Double) {
+    init(id: String, name: String, symbol: String, media: CurrencyMedia, priceUSD: Double, percentChangeUSDLast24Hours: Double, currentCapitalizationUSD: Double, currentSupply: Double) {
         self.id = id
         self.name = name
         self.symbol = symbol
         self.media = media
         self.priceUSD = priceUSD
         self.percentChangeUSDLast24Hours = percentChangeUSDLast24Hours
+        self.currentCapitalizationUSD = currentCapitalizationUSD
+        self.currentSupply = currentSupply
     }
     
     init(dto: CurrencyData) {
@@ -30,6 +34,8 @@ struct Currency {
                   symbol: dto.symbol,
                   media: CurrencyMedia.allCases.first { $0.rawValue == dto.name } ?? .other,
                   priceUSD: dto.metrics.marketData.priceUSD,
-                  percentChangeUSDLast24Hours: dto.metrics.marketData.percentChangeUSD24H)
+                  percentChangeUSDLast24Hours: dto.metrics.marketData.percentChangeUSD24H,
+                  currentCapitalizationUSD: dto.metrics.marketCapitalization.currentCapitalizationUSD,
+                  currentSupply: dto.metrics.circulatingSupply.currentSupply)
     }
 }
